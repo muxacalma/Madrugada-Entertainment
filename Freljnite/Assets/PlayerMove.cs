e47ee7ca526e2de629ffb82d54e77897f3b0b772
Juan <muxacalma@gmail.com>
2018-04-12 03:23:10 +0200

    Juan02
    Cambios de cosas

---
+++ Freljnite/Assets/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!isLocalPlayer)
+			return;
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var y = Input.GetAxis("Vertical")*0.1f;
+
+		transform.Translate(x, y, 0);
+	}
+}
